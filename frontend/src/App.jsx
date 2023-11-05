bf5beca89b2f33929c7ea4533319008a17561a57
Tyrone <tyronemguni@gmail.com>
2023-11-05 20:57:07 +0200

    Scanner page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Warning from "./components/Warning";
 import ReportingForm from "./components/ReportingForm";
 import { Routes, Route } from "react-router-dom";
+import Scanner from "./components/Scanner";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<ReportingForm />} />
       <Route path="/warning" element={<Warning />} />
+      <Route path="/scan" element={<Scanner />} />
     </Routes>
   );
 }
---
+++ frontend/src/components/Scanner.jsx
@@ -0,0 +1,41 @@
+import { Card, Avatar, Button, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+export default function Scanner() {
+  const [payload, setPayload] = useState();
+  const onChange = (e) => {
+    setPayload({ ...payload, [e.target.name]: e.target.value });
+  };
+  return (
+    <center style={{ marginTop: "100px" }}>
+      <Card className="max-w-xl">
+        <Avatar img="/Lion2.jpg" rounded size="xl" />
+        <h1 className="text-center text-xl mb-4 mt-3">KRAVEN THE HUNTER</h1>
+        <span className="text-center text-sm text-xl">
+          <b>Scanner</b>
+          <br />
+        </span>
+        <span className="text-center text-sm mb-4">
+          Scan any URL by pasting it into the field below.
+        </span>
+        <form className="flex flex-col gap-4">
+          <div>
+            <TextInput
+              id="url"
+              placeholder="URL"
+              required
+              type="text"
+              onChange={onChange}
+              name="url"
+            />
+          </div>
+        </form>
+        <Button color="warning">
+          Scan A URL
+          <HiOutlineArrowRight className="ml-2 h-5 w-5" />
+        </Button>
+      </Card>
+    </center>
+  );
+}
