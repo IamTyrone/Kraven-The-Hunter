8cfddecae6c330db9c63ec24aeb6670e5e58aa65
Tyrone <tyronemguni@gmail.com>
2023-11-11 13:35:32 +0200

    Adding numerical features to the Malware dataset

--- engine/features/features.py
+++ engine/features/features.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re, logging
 
 
 class Cleaner:
@@ -10,23 +11,73 @@ class Cleaner:
         digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
         counter = 0
 
-        for value in string:
-            if value in digits:
+        for value in iter(string):
+            if value in digits is True:
                 counter += 1
 
         return counter
+    
+    def special_character_count(self, string: str) -> int:
+        special_chars = ['@','?','-','=','.','#','%','+','$','!','*',',','//']
+        count = 0
+
+        for char in iter(string):
+            if char in special_chars:
+                count += 1
+
+        return count
+    
+    def shortining_service(self, url):
+        match = re.search('bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|'
+                        'yfrog\.com|migre\.me|ff\.im|tiny\.cc|url4\.eu|twit\.ac|su\.pr|twurl\.nl|snipurl\.com|'
+                        'short\.to|BudURL\.com|ping\.fm|post\.ly|Just\.as|bkite\.com|snipr\.com|fic\.kr|loopt\.us|'
+                        'doiop\.com|short\.ie|kl\.am|wp\.me|rubyurl\.com|om\.ly|to\.ly|bit\.do|t\.co|lnkd\.in|'
+                        'db\.tt|qr\.ae|adf\.ly|goo\.gl|bitly\.com|cur\.lv|tinyurl\.com|ow\.ly|bit\.ly|ity\.im|'
+                        'q\.gs|is\.gd|po\.st|bc\.vc|twitthis\.com|u\.to|j\.mp|buzurl\.com|cutt\.us|u\.bb|yourls\.org|'
+                        'x\.co|prettylinkpro\.com|scrnch\.me|filoops\.info|vzturl\.com|qr\.net|1url\.com|tweez\.me|v\.gd|'
+                        'tr\.im|link\.zip\.net',
+                        url)
+        if match:
+            return 1
+        else:
+            return 0
 
 
     def add_features(self):
         df = pd.read_csv(self.data_path)
+        url_length = []
+        verdict = []
+        digit_quantity = []
+        numerical_percantage = []
+        special_character_count = []
+        special_character_percantge = []
+        has_shortining_service = []
+        
 
         for index, row in df.iterrows():
-            df["url_length"] = len(row["url"])
-            df["verdict"] = 1 if row["label"] == "bad" else 0
-            df["digit_quantity"] = self.digit_quantity(row["url"])
+            print(index)
+            url_length.append(len(row["url"]))
+            verdict_value = 1 if row["label"] == "bad" else 0
+            verdict.append(verdict_value)
+            digit_quantity.append(self.digit_quantity(row["url"]))
+            numerical_percantage.append(self.digit_quantity(row["url"])/len(row["url"]))
+            special_character_count.append(self.special_character_count(row["url"]))
+            special_character_percantge.append(self.special_character_count(row["url"])/len(row["url"]))
+            has_shortining_service.append(self.shortining_service(row["url"]))
 
+        df["url_length"] = url_length
+        df["digit_quantity"] = digit_quantity
+        df["numerical_percantage"] = numerical_percantage
+        df["special_character_count"] = special_character_count
+        df["special_character_percantge"] = special_character_percantge
+        df["has_shortining_service"] = has_shortining_service
+        df["verdict"] = verdict
+
+        logging.warning("Note that the CSVs might not appear visually as they do programmatically.")
+
+        df.to_csv("features/prepped/Malware.csv", index=False)
                  
 
 
-cleaner = Cleaner(data_path="./data/Malware.csv")
+cleaner = Cleaner(data_path="features/data/Malware.csv")
 cleaner.add_features()
