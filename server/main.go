1c1b42174222ed796afe20d64634ab61ad290dea
Tyrone <tyronemguni@gmail.com>
2023-11-13 23:13:35 +0200

    Golang Fiber server initialization

--- server/main.go
+++ server/main.go
@@ -1,7 +1,16 @@
 package main
 
+import "github.com/gofiber/fiber/v2"
+
+
 
 
 func main()  {
-	/* Server entry point */
+	app := fiber.New()
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello, World!")
+    })
+
+    app.Listen(":8080")
 }
