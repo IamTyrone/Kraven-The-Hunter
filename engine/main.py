6a64c7871650adb80c9e32e60b2f57d22dfe4c83
Tyrone <tyronemguni@gmail.com>
2026-02-21 02:44:48 +0200

    Disable CORS credentials to fix cross-origin request handling.

--- engine/main.py
+++ engine/main.py
@@ -31,7 +31,7 @@ app = FastAPI(title="Kraven Threat Detection API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
